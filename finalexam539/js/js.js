c33b0b99dcdcfa24369cea5fc09054a53f1b1523
Jane Z <janezio@umich.edu>
2018-04-16 11:25:17 -0400

    more

---
+++ finalexam539/js/js.js
@@ -0,0 +1,15 @@
+$(document).ready(function(){
+    $("#removeimage").click(function(){
+        $("#toremove").hide();
+        $("#tocenter").css({'margin':'auto auto','width':'50%'})
+
+    });
+
+
+    $("#appendimage").click(function(){
+        $("#toremove").show();
+
+
+    });
+
+});
